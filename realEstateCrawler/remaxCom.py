36fcea269937e6ad64bbace98723078bded3ebaf
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-10-08 20:55:20 -0400

    add data to array, add requirements file

--- realEstateCrawler/remaxCom.py
+++ realEstateCrawler/remaxCom.py
@@ -8,26 +8,30 @@ load_dotenv()
 remax_url = os.environ["REMAX_URL"]
 
 def crawlRemaxCom():
+    listingArray = []
+
     driver = webdriver.Chrome()
     options = Options()
     options.add_argument('--proxy-server="164.132.170.100:80"')
     driver.get(remax_url)
 
-    driver.implicitly_wait(4.0)
-    print("after wait")
+    driver.implicitly_wait(4.0) # look up later
 
     listings = driver.find_elements(By.CSS_SELECTOR, 'div[data-test="listing-card"]')
-    print("after listings")
     for listing in listings:
+        currentListing = {}
         address = listing.find_element(By.CSS_SELECTOR, ".card-full-address a").get_attribute("href")
-        print(f"address {address}")
-        price = listing.find_element(By.CSS_SELECTOR, "h4.price").get_attribute("textContent")
-        print(f"PRICE IS {price}")
-        stats = listing.find_elements(By.CLASS_NAME, "card-details-stats")
+        currentListing['address'] = address
 
+        price = listing.find_element(By.CSS_SELECTOR, "h4.price").get_attribute("textContent")
+        currentListing['price'] = price
+
+        stats = listing.find_elements(By.CLASS_NAME, "card-details-stat") # see if this works
         for stat in stats:
             num = stat.find_element(By.TAG_NAME, "strong").get_attribute("textContent")
             unit = stat.find_element(By.TAG_NAME, "span").get_attribute("textContent")
-            print(f"{num} {unit}")
+            currentListing[unit] = num
+        
+        listingArray.append(currentListing)
 
     driver.quit()
