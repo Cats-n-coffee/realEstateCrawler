b29f15c874c8f7c99cfc102211d781a82c8b7d09
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-10-18 18:21:32 -0400

    add basic db operations, need fix the file call

---
+++ realEstateCrawler/db/dbOperations.py
@@ -0,0 +1,57 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
+
+load_dotenv()
+db_connection = os.environ["DB_CONNECTION"]
+
+def read_listing_by_mls(cursor, mls_number):
+    print(f"Lisitng read {mls_number}")
+    try:
+        cursor.execute(
+            "SELECT MlsNumber,StreetAddress,City,ZipCode FROM Listings WHERE [MlsNumber] = ?",
+            (mls_number)
+        )
+        row = cursor.fetchall()
+        
+        if row is None:
+            return False
+        
+        return row
+    except Exception as e:
+        print(f"EXCEPTION READ {e}")
+
+
+def insertListing(cursor, listing):
+    try:
+       cursor.execute(
+            "INSERT INTO Listings"
+            "(MlsNumber, StreetAddress, City, ZipCode, Price, Beds, Baths, Sqft,"
+            "Link, Site)"
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (listing['mls_number'], listing['street_address'], listing['city'], listing['zip'],
+            listing['price'], listing['beds'], listing['baths'], listing['sqft'],
+            listing['link'], listing['site'])
+        )
+    except Exception as e:
+        print(f"EXCEPTION INSERT {e}")
+
+def make_connection():
+    connection = pyodbc.connect(db_connection)
+    cursor = connection.cursor()
+
+    return cursor
+
+def main(data):
+    cursor = make_connection();
+
+    # for listing in data:
+        # read the listing by mlsnumber
+        # if exists update
+        # else insert
+    # insertListing(cursor, data[0])
+    
+    # cursor.execute("SELECT * FROM Listings")
+
+    # for row in cursor:
+    #     print('row = %r' % (row,))
--- realEstateCrawler/main.py
+++ realEstateCrawler/main.py
@@ -1,4 +1,8 @@
 from remaxCom import crawl_remax_com
+from helpers.cleanData import clean_data_from_json
+from db.dbOperations import main
 
 if __name__ == "__main__":
-    crawl_remax_com()
+    # crawl_remax_com()
+    data = clean_data_from_json("listingResults/remaxListings.json")
+    main(data)
